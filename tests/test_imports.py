ca70888cc2acfbafb0da3f9ce85a4177de73b16f
Ruslan Baikulov <ruslan1123@gmail.com>
2022-11-22 20:06:51 +0300

    test: Add more imports

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,17 +1,22 @@
-import pytest
-
-
 class TestImports:
 
     def test_imports(self):
-        import argus
-        import efficientnet_pytorch
-        import kornia
-        import pretrainedmodels
-        import pytorch_toolbelt
-        import segmentation_models_pytorch
-        import tensor_stream
-        import timm
+        import numpy
+        import cv2
+        import numba
+
+        import tensorrt
         import torch
         import torchvision
         import torchaudio
+
+        import ignite
+        import argus
+        import pretrainedmodels
+        import efficientnet_pytorch
+        import pytorch_toolbelt
+        import kornia
+        import timm
+        import segmentation_models_pytorch
+
+        import tensor_stream
