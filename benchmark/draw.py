d10c1fedbf7408aaa660fb198bfddcec791263e2
Vladimir Smirnov <v.smirnov@sportradar.com>
2024-07-17 14:58:44 +0300

    fix: Fix PR comments.

--- benchmark/draw.py
+++ benchmark/draw.py
@@ -2,16 +2,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-bar_width = 0.35  # Width of the bars
-versions = [
-    "23.12-pytorch",
-    "24.06-gpu.pytorch",
-]
+width = 0.35  # Width of the bars
+versions = ["23.12-pytorch", "24.06-gpu.pytorch"]
 
 
 models = None
 train_values, infer_values = [], []
-for i, version in enumerate(versions):
+for version in versions:
     # Load the data from CSV file
     df = pd.read_csv(f"results/benchmark_float32_dokai:{version}.csv")
 
@@ -23,26 +20,19 @@ for i, version in enumerate(versions):
 
 # Plotting
 fig, axis = plt.subplots(1, 2, figsize=(16, 8))
-for i in range(2):
+for i, (label, values) in enumerate([("Training", train_values), ("Inference", infer_values)]):
 
-    index = range(len(models))
+    indices = range(len(models))
 
-    if i == 0:
-        values = train_values
-    else:
-        values = infer_values
-
-    axis[i].bar(index, values[0], bar_width, label=versions[0], color="blue")
-    axis[i].bar([i + bar_width for i in index], values[1], bar_width, label=versions[1], color="green")
+    axis[i].bar(indices, values[0], width, label=versions[0], color="blue")
+    axis[i].bar([i + width for i in indices], values[1], width, label=versions[1], color="green")
 
     # Adding labels and title
     axis[i].set_xlabel("Model")
     axis[i].set_ylabel("Samples per Second")
-    if i == 0:
-        axis[i].set_title(f"Training Samples per Second Comparison")
-    else:
-        axis[i].set_title(f"Inference Samples per Second Comparison")
-    axis[i].set_xticks([i + bar_width / 2 for i in index])
+    axis[i].set_title(f"{label} Samples per Second Comparison")
+    axis[i].set_title(f"{label} Samples per Second Comparison")
+    axis[i].set_xticks([j + width / 2 for j in indices])
     axis[i].set_xticklabels(models, rotation=45, ha="right")
     axis[i].legend()
 
